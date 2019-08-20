31f08a13ce9303f7d761f41961fbf84d9414aec1
AKINOLA Victor <akinolavictor26@gmail.com>
2019-08-20 07:20:52 +0100

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,5 +1,7 @@
 let rand = () => Math.floor(Math.random()*10)+1;
 
+// There are other functionalities like "all" and "race", and so on
+
 let p1 = new Promise((resolve,reject) => { 
     //let errr =() => console.log('there is an error');
     let xo = rand();
