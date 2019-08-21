b52d82dfc1069a221090a2ab7a7962042ca157cd
AKINOLA Victor <akinolavictor26@gmail.com>
2019-08-21 06:08:54 +0100

    Add files via upload

---
+++ Work to do/ajax.js
@@ -0,0 +1,41 @@
+
+const uri = 'http://jsonplaceholder.typicode.com/users';
+
+
+let options = {
+    method: 'GET',
+    mode: 'cors'
+}
+let req = new Request(uri, options);
+
+
+fetch( req )
+    .then( (response) => {
+        if (response.ok){
+           return response.json(); 
+        }else {
+            throw new Error('There is an error');
+        }
+        
+    })
+    .then((data) => {
+        let ul = document.querySelector('#users');
+        let df = new DocumentFragment();
+        //console.log(data);
+        data.forEach((user)=>{
+            let li = document.createElement('li');
+            let pn = document.createElement('p');
+            let pue = document.createElement('p');
+            pn.textContent= user.name;
+            pue.textContent = '' + user.username + ' - ' + user.email;
+            pn.className = 'name';
+            pue.classList.add('info');
+            li.appendChild(pn);
+            li.appendChild(pue);
+            df.appendChild(li);
+        });
+        ul.appendChild(df);
+    })
+    .catch( (err) => {
+        console.log('ERROR; ', err.message);
+    });
