a11d43ca8a9dee0125d08844a69f58d818519371
AKINOLA Victor <akinolavictor26@gmail.com>
2019-08-22 06:46:59 +0100

    Update send_data.js

--- send_data.js
+++ send_data.js
@@ -8,6 +8,7 @@ let formdata = new FormData();
 formdata.append('userid', 3);
 formdata.append('Title','This is my title');
 formdata.append('body', 'This is the body text of the post');
+formdata.append('Description', );
 
 let options= {
     method: 'POST',
