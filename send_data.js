582113a0a4616679cb6764fae95bc411605ca202
AKINOLA Victor <akinolavictor26@gmail.com>
2019-10-17 06:02:07 +0100

    Update send_data.js

--- send_data.js
+++ send_data.js
@@ -1,5 +1,5 @@
 //here we are learning how to send data online/or to an API
-//you should get a response after sending
+//you should get a response after sending the data
 const root = 'http://jsonplaceholder.typicode.com/';
 let uri = root + 'posts';
 
